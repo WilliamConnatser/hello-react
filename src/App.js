756c9708a1d5aa0022598eff35ce548818d2b719
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-03-10 22:06:37 -0500

    first React app with routes setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import Info from './Components/Info'
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import User from './Components/User';
+import Account from './Components/Account';
+import Blog from './Components/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Info />
-        </header>
-      </div>
+      <Router>
+        <div>
+          <div>
+            <Link to="/">Home</Link> |-| 
+            <Link to="/account">Account </Link> |-| 
+            <Link to="/blog">Blog</Link>
+          </div>
+          <Route exact path="/" component={User} />
+          <Route path="/account" component={Account} />
+          <Route path="/blog" component={Blog} />
+        </div>
+      </Router>
     );
   }
 }
